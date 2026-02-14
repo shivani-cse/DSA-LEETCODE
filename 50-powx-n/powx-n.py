a741d85063616ab8f46439e11931129154ef8044
Y Shivani <shivaniyenugundla@gmail.com>
2026-02-14 18:13:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (58.77%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,14 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n==0:
+            return 1
+        if n<0:
+            x=1/x
+            n=-n
+        ans=1
+        while n>0:
+            if n%2==1:
+                ans=ans*x
+            x=x*x
+            n=n//2
+        return ans        
